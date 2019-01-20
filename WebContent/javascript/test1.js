8b25cb32135e0867db6cde105dc8e4783c7accb4
shohei_desk <shohei_desk@192.168.10.7>
2019-01-21 04:28:55 +0900

    create

---
+++ WebContent/javascript/test1.js
@@ -0,0 +1,37 @@
+/**
+ *
+ */
+var canvas;
+var ctx;
+
+onload = function() {
+	  /* canvas要素のノードオブジェクト */
+	  canvas = document.getElementById('canvas1');
+	  /* canvas要素の存在チェックとCanvas未対応ブラウザの対処 */
+	  if ( ! canvas || ! canvas.getContext ) {
+	    return false;
+	  }
+	  drawGrid();
+};
+function drawGrid() {
+
+  /* 2Dコンテキスト */
+  ctx = canvas.getContext('2d');
+  /* グリッド線を描く */
+  var interval = 10;
+  //ctx.lineWidth = 0.5;
+  canvasHeight = canvas.height;
+  canvasWidth = canvas.width;
+  ctx.beginPath();
+  for (var i = 0; i*interval <= canvasHeight; i++){
+    ctx.moveTo(i*interval, 0);
+    ctx.lineTo(i*interval, canvasWidth)
+  }
+
+  for (var i = 0; i*interval <= canvasWidth; i++){
+    ctx.moveTo(0, i*interval);
+    ctx.lineTo(canvasHeight, i*interval)
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
